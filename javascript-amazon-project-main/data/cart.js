0d7de886b94949e009e9c11ccaf89a6370238cc4
kalpeshb2004 <kalpeshbhure01@gmail.com>
2025-11-16 11:54:37 +0530

    made just change

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,25 +1,34 @@
-export  const cart = [];
+export const cart = [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 2,
+  },
+];
 
 //we are creating function addToCart and inserting all the code within this function.
-    export function addToCart(productId) {
-      //data set is an atrributes .
-      /* in above code same type of product was added seprately instead of adding its quantity.
+export function addToCart(productId) {
+  //data set is an atrributes .
+  /* in above code same type of product was added seprately instead of adding its quantity.
    so we created a foreach loop for that
    */
-      let matchingItem;
+  let matchingItem;
 
-      cart.forEach((item) => {
-        if (item.productId === productId) {
-          matchingItem = item;
-        }
-      });
-
-      if (matchingItem) {
-        matchingItem.quantity += 1;
-      } else {
-        cart.push({
-          productId: productId,
-          quantity: 1,
-        });
-      }
+  cart.forEach((item) => {
+    if (item.productId === productId) {
+      matchingItem = item;
     }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+    });
+  }
+}
